f3cdbcbb13c4e5aab798b0f6733e5c53e3dcb6b0
jetta <jettaphoom@gmail.com>
2025-01-22 15:24:50 +0700

    016 [add] new folder

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <main
     class="min-h-screen flex items-center justify-center bg-green-400 text-black font-mono"
   >
-    <h1 class="text-2xl">Hello Vue</h1>
+    <h1 class="text-2xl">Project1 GongGangGaiGonk</h1>
   </main>
 </template>
 
