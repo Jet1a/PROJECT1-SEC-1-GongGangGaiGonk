23d54544560d32f89fe498a3293860e3050a7661
jetta <jettaphoom@gmail.com>
2025-01-22 14:21:29 +0700

    016 [add] vue-template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main
+    class="min-h-screen flex items-center justify-center bg-green-400 text-black font-mono"
+  >
+    <h1 class="text-2xl">Hello Vue</h1>
+  </main>
+</template>
+
+<style scoped></style>
